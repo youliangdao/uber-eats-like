3c84567ab75f56bd356c95a08e068b264cf28f2e
youliangdao <youliangdaojing5@gmail.com>
2022-10-24 21:19:25 +0900

    ✨feat: LineFoodsControllerで仮注文データを一覧表示する機能を実装

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -21,7 +21,17 @@ module Api
       end
 
       def index
-
+        line_foods = LineFood.active
+        if line_foods.exists?
+          render json: {
+            line_food_ids: line_foods.map { |line_food| line_food.id },
+            restaurant: line_foods[0].restaurant,
+            count: line_foods.sum { |line_food| line_food[:count] },
+            amount: line_foods.sum { |line_food| line_food.total_amount }
+          }
+        else
+          render json: {}, status: :no_content
+        end
       end
 
       def replace
