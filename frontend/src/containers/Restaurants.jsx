85f72fd061582ef352387845eff631c23759cdd7
youliangdao <youliangdaojing5@gmail.com>
2022-10-25 21:40:36 +0900

    ✨feat: レストラン一覧ページのヘッダーを作成

--- frontend/src/containers/Restaurants.jsx
+++ frontend/src/containers/Restaurants.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
 import { useEffect } from 'react'
+import styled from "styled-components";
+
+{/** API */}
 import { fetchRestaurants } from '../apis/restaurants'
+
+{/** images */}
+import MainLogo from "../images/logo.png";
+import MainCoverImage from "../images/main-cover-image.png";
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 8px 32px;
+`;
+
+const MainLogoImage = styled.img`
+  height: 90px;
+`;
+
+const MainCoverImageWrapper = styled.div`
+  text-align: center;
+`;
+
+const MainCover = styled.img`
+  height: 600px;
+`;
 
 const Restaurants = () => {
   useEffect(() => {
@@ -8,8 +33,16 @@ const Restaurants = () => {
       console.log(data)
     })
   }, [])
+
   return (
-    <div>Restaurants</div>
+    <>
+      <HeaderWrapper>
+        <MainLogoImage src={MainLogo} alt="main logo"/>
+      </HeaderWrapper>
+      <MainCoverImageWrapper>
+        <MainCover src={MainCoverImage} alt="main cover" />
+      </MainCoverImageWrapper>
+    </>
   )
 }
 
